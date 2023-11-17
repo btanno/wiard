2ae0a983de4ae43081aee1960e12db49222b0144
btanno <149578422+btanno@users.noreply.github.com>
2023-11-17 10:48:05 +0900

    Event::NcHitTestの例を追加

---
+++ examples/nc_hittest.rs
@@ -0,0 +1,16 @@
+fn main() -> anyhow::Result<()> {
+    let mut event_rx = wiard::EventReceiver::new();
+    let _window = wiard::Window::builder(&event_rx)
+        .title("wiard nc_hittest")
+        .hook_nc_hittest(true)
+        .build()?;
+    loop {
+        let Some((event, _)) = event_rx.recv() else {
+            break;
+        };
+        if let wiard::Event::NcHitTest(t) = event {
+            t.set(Some(wiard::event::NcHitTestValue::Caption));
+        }
+    }
+    Ok(())
+}
