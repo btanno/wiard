7cd13dcd92f83f072e437d32a02aa3acc4653f0c
btanno <149578422+btanno@users.noreply.github.com>
2026-02-23 23:31:52 +0900

    UiThreadにBuilderを追加

---
+++ tests/panic_ui_thread_on_main_loop_end.rs
@@ -0,0 +1,18 @@
+#[test]
+#[should_panic]
+fn ui_thread_on_main_loop_end() {
+    wiard::UiThread::new()
+        .on_main_loop_end(|| {
+            panic!();
+        })
+        .build();
+    let mut event_rx = wiard::EventReceiver::new();
+    let window = wiard::Window::builder(&event_rx).visible(false).build().unwrap();
+    window.close();
+    loop {
+        let Some(_) = event_rx.recv() else {
+            break;
+        };
+    }
+    wiard::UiThread::join().unwrap();
+}
